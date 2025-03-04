fbd5c3198d63530b3b9ddc1f8476978a2ab31218
Ludwig <ludwig@frostudios.com>
2025-03-04 13:29:51 +0100

    removed cloned repoand changed functions

--- src/files/check_file_content.go
+++ src/files/check_file_content.go
@@ -21,17 +21,35 @@ func CheckFileContent(dirPath string, typeOfFile string) ([]string, error) {
 			lowerFileName := strings.ToLower(d.Name())
 			lowerPath := strings.ToLower(path)
 
+			fmt.Println(lowerFileName)
+
 			switch lowerFileType {
 			case "gitignore":
 				if lowerFileName == ".gitignore" {
 					matchedFiles = append(matchedFiles, path)
 				}
 			case "readme":
-				if strings.HasSuffix(lowerFileName, "readme") {
+				// Ensure "README" is the prefix and check for valid suffixes
+				if (strings.HasPrefix(lowerFileName, "readme") || strings.HasPrefix(lowerFileName, "readme.")) &&
+					(strings.HasSuffix(lowerFileName, ".md") ||
+						strings.HasSuffix(lowerFileName, ".txt") ||
+						strings.HasSuffix(lowerFileName, ".rst") ||
+						strings.HasSuffix(lowerFileName, ".html")) {
+					matchedFiles = append(matchedFiles, path)
+				}
+			case "license":
+				// Check if the file name starts with "license" and matches an acceptable extension
+				if (strings.HasPrefix(lowerFileName, "license") ||
+					strings.HasPrefix(lowerFileName, "LICENSE")) &&
+					(strings.HasSuffix(lowerFileName, ".txt") ||
+						strings.HasSuffix(lowerFileName, ".md") ||
+						strings.HasSuffix(lowerFileName, ".rst")) {
 					matchedFiles = append(matchedFiles, path)
 				}
 			case "workflow":
-				if strings.Contains(lowerPath, ".github/workflows/") && (strings.HasSuffix(lowerFileName, ".yml")) || (strings.HasSuffix(lowerFileName, ".yaml")) {
+				// Ensure the file is within ".github/workflows/" and ends with .yml or .yaml
+				if strings.Contains(lowerPath, ".github/workflows/") &&
+					(strings.HasSuffix(lowerFileName, ".yml") || strings.HasSuffix(lowerFileName, ".yaml")) {
 					matchedFiles = append(matchedFiles, path)
 				}
 			case "test":
